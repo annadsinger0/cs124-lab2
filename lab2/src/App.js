16c85d4ed55b987ebaf98f1a31a24efc7fddd56c
annadsinger0 <asinger@hmc.edu>
2022-02-27 21:09:20 -0800

    working lab 1 in react

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import {Fragment} from "react";
+import checkboxEmpty from "./assets/checkboxEmpty.png";
+import checkboxFull from "./assets/checkboxFull.png";
+import trashcan from "./assets/trashcan.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Fragment>
+      <head>
+        <title>ToDo</title>
+        <link rel="stylesheet" href="style.css"/>
+      </head>
+  <body>
+
+  <h1>ToDo</h1>
+
+  <div id="tools">
+    <label htmlFor="completed-item-show" id="completed-item-show-label">
+      <span>Show completed items</span>
+      <img src={checkboxFull} className="checkbox" id="completed-item-show-check"/>
+    </label>
+    <input type="checkbox" id="completed-item-show"/>
+      <img src={trashcan} id="trash"/>
+  </div>
+
+
+  <ul id="todolist">
+    <li>
+      <img src={checkboxEmpty} className="checkbox"/>
+      <p>Buy new John Grisham book</p>
+    </li>
+    <li>
+      <img src={checkboxFull} className="checkbox"/>
+      <p>another lowercase task</p>
+    </li>
+    <li>
+      <img src={checkboxEmpty} className="checkbox"/>
+      <p>SOME REALLY IMPORTANT UPPERCASE TASK</p>
+    </li>
+  </ul>
+
+  <div id="new-task">
+    <input type="button" value="+" id="new-task-button"/>
+      <input type="text" id="new-task-input" placeholder="Add task"/>
+
+  </div>
+
+
+  </body>
+      </Fragment>);
 }
 
 export default App;
